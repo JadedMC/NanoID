5c87c0157ff1112bbe9d857b8a8582f8ccf1fae2
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-05-08 14:35:22 -0400

    Add some comments

--- src/main/java/net/jadedmc/nanoid/NanoID.java
+++ src/main/java/net/jadedmc/nanoid/NanoID.java
@@ -38,48 +38,105 @@ public class NanoID {
     public static final SecureRandom DEFAULT_RANDOM = new SecureRandom();
     private final byte[] bytes;
 
+    /**
+     * Creates a NanoID with all default values. Generates a similar number of possible Ids as UUID v4.
+     * Generator: SecureRandom
+     * Size: 21
+     * Alphabet: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-"
+     */
     public NanoID() {
         this(DEFAULT_RANDOM, DEFAULT_SIZE, DEFAULT_ALPHABET);
     }
 
+    /**
+     * Creates a NanoID with the default generator and alphabet, but with a given size.
+     * @param size Size of the NanoID.
+     */
     public NanoID(final int size) {
         this(DEFAULT_RANDOM, size, DEFAULT_ALPHABET);
     }
 
+    /**
+     * Creates a NanoID with the default generator, with a given size and alphabet.
+     * @param size Size of the NanoID.
+     * @param alphabet Alphabet the NanoID uses.
+     */
     public NanoID(final int size, final String alphabet) {
         this(DEFAULT_RANDOM, size, alphabet);
     }
 
+    /**
+     * Creates a NanoID with a given generator, size, and alphabet.
+     * @param random Random Number Generator to create the NanoID with.
+     * @param size Size of the NanoID.
+     * @param alphabet Alphabet the NanoID uses.
+     */
     public NanoID(final Random random, final int size, final String alphabet) {
         this(generateNanoIDString(random, size, alphabet).getBytes(StandardCharsets.UTF_8));
     }
 
+    /**
+     * Creates a NanoID with a given byte array.
+     * @param bytes Byte array that makes up the NanoID.
+     */
     public NanoID(final byte[] bytes) {
         this.bytes = bytes;
     }
 
+    /**
+     * Converts the NanoID into it's byte form.
+     * @return Byte representation of the NanoID.
+     */
     public final byte[] asByteArray() {
         return bytes;
     }
 
+
+    /**
+     * Compares an object to this NanoID.
+     * Only returns true if the object is a NanoID and has the same byte array
+     * @param object Object to be compared.
+     * @return true if the objects are the same; false otherwise.
+     */
     @Override
-    public boolean equals(Object object) {
+    public boolean equals(final Object object) {
+        // Null-objects cannot be equal to the NanoID.
+        if(object == null) {
+            return false;
+        }
+
+        // Non-NanoID objects cannot be equal to the NanoID.
         if(!(object instanceof NanoID)) {
             return false;
         }
 
-        final NanoID other = (NanoID) object;
-        return Arrays.equals(other.asByteArray(), this.bytes);
+        // Check if the bytes match.
+        final NanoID otherID = (NanoID) object;
+        return Arrays.equals(otherID.asByteArray(), this.bytes);
     }
 
+    /**
+     * Gets the length of the ID (in number of bytes).
+     * @return Number of bytes that make up the ID.
+     */
     public final int size() {
         return this.bytes.length;
     }
 
+    /**
+     * Converts the NanoID into its String form.
+     * @return A String representation of the NanoID.
+     */
     @Override
     public String toString() {
         return new String(bytes, StandardCharsets.UTF_8);
     }
+
+    /**
+     * Get an NanoID object from its representative String.
+     * @param string String representing the NanoID.
+     * @return NanoID that is represented by the given String.
+     */
     public static NanoID fromString(final String string) {
         return new NanoID(string.getBytes(StandardCharsets.UTF_8));
     }
@@ -91,12 +148,10 @@ public class NanoID {
         final StringBuilder idBuilder = new StringBuilder();
 
         while (true) {
-
             final byte[] bytes = new byte[step];
             random.nextBytes(bytes);
 
             for (int i = 0; i < step; i++) {
-
                 final int alphabetIndex = bytes[i] & mask;
 
                 if (alphabetIndex < alphabet.length()) {
